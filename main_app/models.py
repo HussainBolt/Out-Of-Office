5aab36183329e786ace313a4f73b11877fa37f13
schen044 <ninjarulers@yahoo.com>
2022-10-28 11:26:19 -0700

    Fixes CSS

--- main_app/models.py
+++ main_app/models.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-#Michael Alberto was here!!!!
-# Shong was here
+class Trip(models.Model):
+    name = models.CharField(max_length=100)
+    locations = models.TextField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField()
