e3e45849923a086349c9ac7f2d14f194da3eacb3
mikealberto <alberto.michael1701@gmail.com>
2022-10-31 12:36:41 -0400

    URL/Routes for Itinerary. Which is many to many relationship

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path("trips/<int:pk>/update/", views.TripUpdate.as_view(), name="trips_update"),
     path("trips/<int:pk>/delete/", views.TripDelete.as_view(), name="trips_delete"),
     path('accounts/signup/', views.signup, name='signup'),
+    #URL FOR Itinerary
+    #NOTe :comment it out so it does not affect your code
+    path("itineraries/", views.ItineraryList.as_view(), name="itineraries_index"),
+    path("itineraries/<int:pk>/", views.IntineraryDetail.as_view(), name="itineraries_detail"), 
+    path("itineraries/create/", views.ItineraryCreate.as_view(), name="itineraries_create"),
+    path("itineraries/<int:pk>/update/", views.ItineraryUpdate.as_view(), name="itineraries_update"),
+    path("itineraries/<int:pk>/delete/", views.ItineraryDelete.as_view(), name="itineraries_delete"),
+
 ]
