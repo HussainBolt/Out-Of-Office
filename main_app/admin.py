a11595114fe94850d6d6cc0cb7ac085a799acfe6
schen044 <ninjarulers@yahoo.com>
2022-10-31 10:18:14 -0700

    Registers Itinerary and Activity to admin page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Trip
+from .models import Trip, Itinerary, Activity
 
 # Register your models here.
 admin.site.register(Trip)
+admin.site.register(Itinerary)
+admin.site.register(Activity)
